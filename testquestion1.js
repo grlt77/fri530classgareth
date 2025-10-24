f67893bd0dc13cc8573c723beaf7621ecfbfc02c
grlt77 <194495867+grlt77@users.noreply.github.com>
2025-10-24 10:40:34 +0000

    24 10 2025

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,24 @@
+let posX, xSpeed;
 
+function setup(){
+    createCanvas(600, 400);
+    background("blue")
+    posX = width/ 2;
+    xSpeed = 5;
+    fill('limegreen');
+}
+
+function draw(){
+    background("blue")
+    circle(posX,height/2,50)
+    constrain(posX,0,600)
+    if (posX < 575){
+        posX = posX + xSpeed
+    }
+
+    
+    if (posX === 575 || posX === 0){
+        xSpeed = xSpeed * -1
+        posX = posX + xSpeed
+    }
+}
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,17 @@
+let pokemons = ["Pikachu","Raichu","Bulbasaur","Mewtwo","Charizard"];
+let yPos;
 
+function setup(){
+    createCanvas(200, 300);
+    fill("pink");
+    textAlign(CENTER,CENTER)
+    textSize(20)
+    yPos = 100;
+}
+function draw(){
+    for (let y = 0;y < 10; y++){
+        yPos = yPos + y * 19
+        text(pokemons[y],150,yPos)
+        
+    }
+}
